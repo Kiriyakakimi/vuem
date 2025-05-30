5d8ab5a292bbfc128611d0da20fe96a12602b06b
Kiriyakakimi <147922673+Kiriyakakimi@users.noreply.github.com>
2025-05-30 15:35:22 +0700

    he

--- src/page/all.tsx
+++ src/page/all.tsx
@@ -2,19 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface Movie {
-  id: number;
-  title: string;
-  imageUrl: string;
-  description: string;
+  id: string;
+  name: string;
+  thumbUrl: string;
+  posterUrl: string;
+  modified : number;
 }
 
 const movies: Movie[] = [
-  { id: 1, title: 'Tên phim 1', imageUrl: '/vite.svg',description: 'Mô tả phim 1' },
-  { id: 2, title: 'Tên phim 2', imageUrl: '/vite.svg',description: 'Mô tả phim 2' },
-  { id: 3, title: 'Tên phim 3', imageUrl: '/vite.svg',description: 'Mô tả phim 3' },
-  { id: 4, title: 'Tên phim 4', imageUrl: '/vite.svg',description: 'Mô tả phim 4' },
-  { id: 5, title: 'Tên phim 5', imageUrl: '/vite.svg',description: 'Mô tả phim 5' },
-  { id: 6, title: 'Tên phim 6', imageUrl: '/vite.svg',description: 'Mô tả phim 6' },
+    {"id":"gai-ngan-do-phan-2","name":"Gái Ngàn Đô Phần 2","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/1652236165.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/maxresdefault%20(1).jpg","modified":1709778977000},{"id":"gai-ngan-do-phan-1","name":"Gái Ngàn Đô Phần 1","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/117185648_2763241303949265_3997612971491675570_n.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/maxresdefault(3).jpg","modified":1709778831000},{"id":"sengoku-youko","name":"Sengoku Youko","thumbUrl":"https://phim.nguonc.com/public/images/Film/140627.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/140627.jpg","modified":1709778686000},{"id":"metallic-rouge","name":"Metallic Rouge","thumbUrl":"https://phim.nguonc.com/public/images/Film/140210.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/140210.jpg","modified":1709778684000},{"id":"jaku-chara-tomozaki-kun-2nd-stage","name":"Jaku-Chara Tomozaki-kun 2nd Stage","thumbUrl":"https://phim.nguonc.com/public/images/Film/139378.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/139378.jpg","modified":1709778682000},{"id":"ishura","name":"Ishura","thumbUrl":"https://phim.nguonc.com/public/images/Film/140122.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/140122.jpg","modified":1709778680000},{"id":"the-than-ngu-khi-su-cuoi-cung","name":"Thế Thần: Ngự khí sư cuối cùng","thumbUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-thumb.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-poster.jpg","modified":1709777807000},{"id":"nguoi-sat-3","name":"Người Sắt 3","thumbUrl":"https://phim.nguonc.com/public/images/Film/o0dmWIrF63lBPQVFdvqaWUxQ7xz.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/aFTYFqrWp4RS46Twm87l5e0ItYb.jpg","modified":1709777496000},{"id":"the-than-ngu-khi-su-cuoi-cung-phan-2","name":"Thế Thần: Ngự khí sư cuối cùng (Phần 2)","thumbUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-phan-2-thumb.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-phan-2-poster.jpg","modified":1709777423000},{"id":"nguoi-sat-2","name":"Người Sắt 2","thumbUrl":"https://phim.nguonc.com/public/images/Film/q9L8fMYW4dBQOdDAfbbxte7gciJ.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/jesRqfL9v6HNnowe795xjmuKUXl.jpg","modified":1709747281000},{"id":"the-than-ngu-khi-su-cuoi-cung-phan-3","name":"Thế Thần: Ngự khí sư cuối cùng (Phần 3)","thumbUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-phan-3-thumb.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/the-than-ngu-khi-su-cuoi-cung-phan-3-poster.jpg","modified":1709747200000},{"id":"chao-mung-den-voi-lop-hoc-de-cao-thuc-luc-3","name":"Chào Mừng Đến Với Lớp Học Đề Cao Thực Lực 3","thumbUrl":"https://phim.nguonc.com/public/images/Film/139318.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/139318.jpg","modified":1709746621000},{"id":"nanatsu-no-taizai-mokushiroku-no-yonkishi","name":"Nanatsu no Taizai: Mokushiroku no Yonkishi","thumbUrl":"https://phim.nguonc.com/public/images/Film/138530.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/138530.jpg","modified":1709746557000},{"id":"nguoi-sat","name":"Người Sắt","thumbUrl":"https://phim.nguonc.com/public/images/Film/nguoi-sat-thumb.jpg","posterUrl":"https://phim.nguonc.com/public/images/Post/frcekaro_1920x1080-nguoisat_1920_1080.jpg","modified":1709745775000},{"id":"nguoi-sat-su-noi-gian-cua-technovore","name":"Người Sắt: Sự Nổi Giận Của Technovore","thumbUrl":"https://phim.nguonc.com/public/images/Film/qI9c70BQTIsU9fPJvvv1hsui48P.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/kqMYb35r3NCsFeaV1EfBg3rzZWE.jpg","modified":1709745664000},{"id":"nguoi-sat-vo-dich","name":"Người Sắt Vô Địch","thumbUrl":"https://phim.nguonc.com/public/images/Film/nguoi-sat-vo-dich-thumb.jpg","posterUrl":"https://phim.nguonc.com/public/images/Film/nguoi-sat-vo-dich-poster.jpg","modified":1709745240000},{"id":"truong-tuong-vat","name":"Trường Tương Vật","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/Truong-Tuong-Vat.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/a_100566239_m_601_en_720_405.jpg","modified":1709745116000},{"id":"vinh-an-mong","name":"Vĩnh An Mộng","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/Z8vkN8_4f.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/hq720(1).jpg","modified":1709744676000},{"id":"tu-xuyen-quang-minh-tam-kiet","name":"Tử Xuyên: Quang Minh Tam Kiệt","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/Yg6Q9ol9zJj0.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/a_100548258_m_601_zh-CN_m2_720_405.jpg","modified":1709744657000},{"id":"dai-tuong-quan-phan-1","name":"Đại Tướng Quân Phần 1","thumbUrl":"https://phim.nguonc.com/public/images/PostCat/449498127_shogun-s01.jpg","posterUrl":"https://phim.nguonc.com/public/images/PostCat/shogun-fx-season-1-key-art.jpg","modified":1709744621000}
 ];
 const All: React.FC = () => {
   const [hoveredMovie, setHoveredMovie] = useState<Movie | null>(null);
@@ -60,11 +56,11 @@ const All: React.FC = () => {
               onMouseLeave={() => setHoveredMovie(null)}
             >
               <a href="#">
-              <img src={movie.imageUrl} alt={movie.title} className="w-full h-auto rounded-md mb-2" />
-              <h3 className="text-lg font-semibold">{movie.title}</h3>
+              <img src={movie.thumbUrl} alt={movie.name} className="w-full h-auto rounded-md mb-2" />
+              <h3 className="text-lg font-semibold">{movie.name}</h3>
               {hoveredMovie?.id === movie.id && (
                 <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center p-4 rounded-lg">
-                  <p className="text-sm">{hoveredMovie.description}</p>
+                  <p className="text-sm">{hoveredMovie.name}</p>
                 </div>
               )}
               </a>
